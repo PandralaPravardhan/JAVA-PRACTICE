921f7bc9d275ce6afeb9f7fa31bfe54d2e1003d6
PandralaPravardhan <pravardhanpandrala@gmail.com>
2026-01-29 22:14:57 +0530

    Add files via upload

---
+++ reducezero.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class reducezero {
+
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num = sc.nextInt();  
+        System.out.println(numberOfSteps(num));
+    }
+}
